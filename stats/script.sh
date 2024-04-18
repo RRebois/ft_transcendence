a777cd990125f0529f2cb7f08c2da56a313f5de9
rrebois <rrebois@student.42lyon.fr>
2024-04-18 16:01:42 +0200

    database with tables of userStats completed and operationnal

--- stats/script.sh
+++ stats/script.sh
@@ -12,9 +12,9 @@ then
 fi
 
 # python manage.py flush --no-input
-python3 manage.py makemigrations
-python3 manage.py migrate --noinput
-python3 manage.py collectstatic
-python3 manage.py createsuperuser --noinput
+python manage.py makemigrations
+python manage.py migrate --noinput
+python manage.py collectstatic
+python manage.py createsuperuser --noinput
 
 exec "$@"
