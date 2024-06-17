3f3099168acc8a427fa9b04af16c4ba589f4ff4c
K1000 <camil.bernot.cb@gmail.com>
2024-06-18 01:58:55 +0200

    [front] relaunch round when a player loose and display scores

--- frontend/app/js/views/match.js
+++ frontend/app/js/views/match.js
@@ -1,6 +1,7 @@
 import * as THREE from 'three';
-import { TextGeometry } from 'three/addons/geometries/TextGeometry.js';
-import {FontLoader} from "three/addons";
+import {FontLoader} from 'three/addons/loaders/FontLoader.js';
+import {TextGeometry} from 'three/addons/geometries/TextGeometry.js';
+
 
 export default class Match {
     constructor(props) {
@@ -23,8 +24,8 @@ export default class Match {
         this.ball_x = 0;
         this.ball_y = 0;
         this.ball_radius = 0.3;
-        this.ball_velocity_x = 0.05;
-        this.ball_velocity_y = 0.05;
+        this.ball_velocity_x = 2 * ((Math.random() - 0.5) / 10);  // Double the speed in x direction
+        this.ball_velocity_y = 2 * ((Math.random() - 0.5) / 25);  // Double the speed in y direction
 
         this.scene = new THREE.Scene();
         this.scene.background = new THREE.Color(0xbae6fd);
@@ -43,6 +44,7 @@ export default class Match {
         this.createPaddle('p1');
         this.createPaddle('p2');
         this.createBall();
+        this.printScores();
 
         // Controls
         window.addEventListener('keydown', this.handleKeyEvent.bind(this));
@@ -56,39 +58,28 @@ export default class Match {
             this.renderer.setSize(window.innerWidth, window.innerHeight);
         });
 
-        // const loader = new FontLoader();
-        // loader.load( 'https://threejs.org/examples/fonts/helvetiker_regular.typeface.json', function ( font )
-        // {
-        //
-        //     const matLite = new THREE.MeshBasicMaterial(
-        //         {
-        //             color: 0x2c3e50,
-        //             transparent: true,
-        //             opacity: 0.8,
-        //             side: THREE.DoubleSide
-        //         } );
-        //
-        //     const message = " Three.js\nUniversity";
-        //
-        //     const shapes = font.generateShapes( message, 100 );
-        //
-        //     const geometry = new THREE.ShapeGeometry( shapes );
-        //     geometry.computeBoundingBox();
-        //
-        //     const xMid = - 0.5 * ( geometry.boundingBox.max.x - geometry.boundingBox.min.x );
-        //     geometry.translate( xMid, 0, 0 );
-        //
-        //     const text = new THREE.Mesh( geometry, matLite );
-        //
-        //     text.position.z = 1;
-        //     this.scene.add( text );
-        //
-        //
-        // } ); //end load function
-
         // Animate
         this.animate = this.animate.bind(this);
         this.animate();
+    }
+
+    printScores() {
+        const loader = new FontLoader();
+        loader.load('https://threejs.org/examples/fonts/helvetiker_regular.typeface.json', (font) => {
+            const textGeometry = new TextGeometry(`${this.score_p1}\u00A0-\u00A0${this.score_p2}`, {
+                font: font,
+                size: 3,
+                height: 0.1,
+                curveSegments: 12,
+                bevelEnabled: false
+            });
+            const textMaterial = new THREE.MeshBasicMaterial({color: 0x000000});
+            const text = new THREE.Mesh(textGeometry, textMaterial);
+            text.position.set(0, 0, 0);
+            text.name = 'scores';
+            textGeometry.center();
+            this.scene.add(text);
+        });
     }
 
     handleKeyEvent(event) {
@@ -114,23 +105,23 @@ export default class Match {
 
     createBall() {
         const geometry = new THREE.SphereGeometry(this.ball_radius, 32, 32);
-        const material = new THREE.MeshBasicMaterial({ color: 0xffff00 });
+        const material = new THREE.MeshBasicMaterial({color: 0xffff00});
         const ball = new THREE.Mesh(geometry, material);
         ball.position.set(this.ball_x, this.ball_y, 0);
         ball.name = 'ball';
         this.scene.add(ball);
     }
 
-    createPaddle(player = 'p1')  {
+    createPaddle(player = 'p1') {
         const geometry = new THREE.BoxGeometry(0.2, this.paddle_length, 1);
-        let x = Math.round(-this.stadium_length/2 + 1);
+        let x = Math.round(-this.stadium_length / 2 + 1);
         let color = 0xff0000;
         if (player === 'p2') {
-            x = Math.round(this.stadium_length/2 - 2);
+            x = Math.round(this.stadium_length / 2 - 2);
 
             color = 0x0000ff;
         }
-        const material = new THREE.MeshBasicMaterial({ color: color});
+        const material = new THREE.MeshBasicMaterial({color: color});
         const paddle = new THREE.Mesh(geometry, material);
         paddle.position.set(x, 0, 0);
         paddle.name = player;
@@ -138,15 +129,15 @@ export default class Match {
     }
 
     createStadium() {
-        this.createWall(0, this.stadium_width/2, 0, this.stadium_length, this.stadium_thickness, this.stadium_height);  // up
-        this.createWall(-this.stadium_length/2, 0, 0, this.stadium_thickness, this.stadium_width, this.stadium_height);  // left
-        this.createWall(this.stadium_length/2, 0, 0, this.stadium_thickness, this.stadium_width, this.stadium_height);  // right
-        this.createWall(0, -this.stadium_width/2, 0, this.stadium_length, this.stadium_thickness, this.stadium_height);  // down
+        this.createWall(0, this.stadium_width / 2, 0, this.stadium_length, this.stadium_thickness, this.stadium_height);  // up
+        this.createWall(-this.stadium_length / 2, 0, 0, this.stadium_thickness, this.stadium_width, this.stadium_height);  // left
+        this.createWall(this.stadium_length / 2, 0, 0, this.stadium_thickness, this.stadium_width, this.stadium_height);  // right
+        this.createWall(0, -this.stadium_width / 2, 0, this.stadium_length, this.stadium_thickness, this.stadium_height);  // down
     }
 
     createWall(x, y, z, width, height, depth) {
         const geometry = new THREE.BoxGeometry(width, height, depth);
-        const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+        const material = new THREE.MeshBasicMaterial({color: 0x00ff00});
         const wall = new THREE.Mesh(geometry, material);
         wall.position.set(x, y, z);
         this.scene.add(wall);
@@ -155,13 +146,20 @@ export default class Match {
     updatePaddlePosition(player, y_pos) {
         const paddle = this.scene.getObjectByName(player);
         if (paddle) {
-            const offset = this.paddle_length/2;
-            if ((paddle.position.y + y_pos + offset) > this.stadium_width/2 || (paddle.position.y + y_pos - offset) < -this.stadium_width/2) {
+            const offset = this.paddle_length / 2;
+            if ((paddle.position.y + y_pos + offset) > this.stadium_width / 2 || (paddle.position.y + y_pos - offset) < -this.stadium_width / 2) {
                 return;
             }
-
             paddle.position.y += y_pos;
         }
+    }
+
+    updateScores() {
+        const scoreText = this.scene.getObjectByName('scores');
+        if (scoreText) {
+            this.scene.remove(scoreText);
+        }
+        this.printScores();
     }
 
     animate() {
@@ -192,30 +190,39 @@ export default class Match {
         const paddle1 = this.scene.getObjectByName('p1');
         const paddle2 = this.scene.getObjectByName('p2');
         if (paddle1 && paddle2) {
-            if (ball.position.x - this.ball_radius < paddle1.position.x + 0.2 && ball.position.y <= paddle1.position.y + this.paddle_length/2 && ball.position.y >= paddle1.position.y - this.paddle_length/2) {
+            if (ball.position.x - this.ball_radius < paddle1.position.x + 0.2 && ball.position.y <= paddle1.position.y + this.paddle_length / 2 && ball.position.y >= paddle1.position.y - this.paddle_length / 2) {
                 this.ball_velocity_x = -this.ball_velocity_x;
             }
-            if (ball.position.x + this.ball_radius > paddle2.position.x - 0.2 && ball.position.y <= paddle2.position.y + this.paddle_length/2 && ball.position.y >= paddle2.position.y - this.paddle_length/2) {
+            if (ball.position.x + this.ball_radius > paddle2.position.x - 0.2 && ball.position.y <= paddle2.position.y + this.paddle_length / 2 && ball.position.y >= paddle2.position.y - this.paddle_length / 2) {
                 this.ball_velocity_x = -this.ball_velocity_x;
             }
         }
 
         // Check for collisions with walls
-        if (ball.position.y + this.ball_radius > this.stadium_width/2 || ball.position.y - this.ball_radius < -this.stadium_width/2) {
+        if (ball.position.y + this.ball_radius > this.stadium_width / 2 || ball.position.y - this.ball_radius < -this.stadium_width / 2) {
             this.ball_velocity_y = -this.ball_velocity_y;
         }
         // Player 2 wins the round
-        if (ball.position.x - this.ball_radius < -this.stadium_length/2) {
+        if (ball.position.x - this.ball_radius < -this.stadium_length / 2) {
             this.score_p2++;
-
-            this.ball_velocity_x = -this.ball_velocity_x;
+            this.updateScores();
+            this.newRound();
         }
         // Player 1 wins the round
-        else if (ball.position.x + this.ball_radius > this.stadium_length/2) {
-            // this.ball.position.set(0, 0, 0);
+        else if (ball.position.x + this.ball_radius > this.stadium_length / 2) {
             this.score_p1++;
-            this.ball_velocity_x = -this.ball_velocity_x;
+            this.updateScores();
+            this.newRound();
         }
+    }
+
+    newRound() {
+        this.ball_x = 0;
+        this.ball_y = 0;
+        this.ball_velocity_x = 2 * ((Math.random() - 0.5) / 10);
+        this.ball_velocity_y = 2 * ((Math.random() - 0.5) / 25);
+        const ball = this.scene.getObjectByName('ball');
+        ball.position.set(this.ball_x, this.ball_y, 0);
     }
 
 }
