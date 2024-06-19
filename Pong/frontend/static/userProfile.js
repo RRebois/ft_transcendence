070d15a8c269d36ebef46b2d9868cf96bbb0b871
rrebois <rrebois@student.42lyon.fr>
2024-06-19 17:48:05 +0200

    added toggle for 2FA activation/deactivation

--- Pong/frontend/static/userProfile.js
+++ Pong/frontend/static/userProfile.js
@@ -11,6 +11,7 @@ function    display_user_data(element) {
         // Create expand div
         const   exDiv = document.createElement('div');
         exDiv.setAttribute("id", element.parentElement.parentElement.id + "Child");
+        exDiv.classList.add("displayAnim");
 
         if (element.id === "info") {
             fetch("getUsernameConnected")
@@ -39,7 +40,6 @@ function    display_user_data(element) {
                             append_info(divData, key, user_info[key]);
                         else
                             isStud = user_info[key];
-                    exDiv.classList.add("displayAnim");
                     element.parentElement.parentElement.append(exDiv);
 
                     // run animation
@@ -55,7 +55,7 @@ function    display_user_data(element) {
                         exDiv.classList.remove("displayAnim");
                         img.classList.remove("displayAnimImg");
                     });
-                    exDiv.style.opacity = "1px";
+//                    exDiv.style.opacity = "1px";
 
                     // Add form when editing image is clicked
                     img.addEventListener('click', () => {
@@ -71,31 +71,94 @@ function    display_user_data(element) {
             });
         }
         else if (element.id === "security") {
-            console.log("security");
+            // Change arrow
+            element.classList.remove("fa-square-caret-down");
+            element.classList.add("fa-square-caret-up");
+            element.parentElement.parentElement.append(exDiv);
+
+            // Create div to contain change password and 2FA
+            const   subExDiv = document.createElement('div');
+            // Add content to subExDiv
+            const   togDiv = document.createElement('div');
+            togDiv.className = 'form-check form-switch';
+
+            const   togLabel = document.createElement('label');
+            togLabel.innerHTML = "2FA Deactivated."
+            togLabel.setAttribute('for', 'flexSwitchCheckDefault');
+            togLabel.className = "form-check-label";
+            togLabel.setAttribute('id', 'togLab');
+
+            const   togInput = document.createElement('input');
+            togInput.setAttribute('type', 'checkbox');
+            togInput.setAttribute('id', 'flexSwitchCheckDefault');
+            togInput.className = "form-check-input";
+            togInput.addEventListener('click', () => {
+                if (togLabel.innerHTML === "2FA Deactivated.")
+                    togLabel.innerHTML = "2FA Activated.";
+                else
+                    togLabel.innerHTML = "2FA Deactivated.";
+            });
+
+            togDiv.append(togInput, togLabel);
+            subExDiv.append(togDiv);
+            exDiv.append(subExDiv);
+
+            // Add div with bottom border to see the animation
+//            const   divLine = document.createElement('div');
+//            element.parentElement.parentElement.append(divLine);
+//            divLine.classList.add('txtSectionDiv'); // or create the div from the beginning but hide it and display it on when animation running??
+//            divLine.style.borderBottom = "2px solid black"; // create div like Account security and p. info
+
+            // run animation
+            exDiv.style.animationPlayState = "running";
+            exDiv.addEventListener("animationend", (event) => {
+                // stop animation
+                exDiv.style.animationPlayState = "paused";
+
+                // rm animation class
+                exDiv.classList.remove("displayAnim");
+            });
+//            exDiv.style.opacity = "1px";
         }
     }
     else if (element.classList.contains("fa-square-caret-up"))
     {
-        // Select div displaying infos
-        var divRM = document.getElementById(element.parentElement.parentElement.id + "Child");
-        var img = document.getElementById(element.parentElement.parentElement.id + "img");
+        if (element.id === "info")
+        {
+            // Select div displaying infos
+            var divRM = document.getElementById(element.parentElement.parentElement.id + "Child");
+            var img = document.getElementById(element.parentElement.parentElement.id + "img");
 
-        // Add animation class
-        divRM.classList.add("rmAnim");
-        img.classList.add("rmAnimImg");
+            // Add animation class
+            divRM.classList.add("rmAnim");
+            img.classList.add("rmAnimImg");
 
-        // Changes div opacity
-        divRM.style.opacity = "0px";
+//            divRM.style.opacity = '0px';
+            // run animation
+            divRM.style.animationPlayState = "running";
+            img.style.animationPlayState = "running";
 
-        // run animation
-        divRM.style.animationPlayState = "running";
-        img.style.animationPlayState = "running";
+            // remove div after animation ended
+            divRM.addEventListener("animationend", (event) => {
+                divRM.remove();
+                img.remove();
+            });
+        }
+        else {
+            // Select div displaying infos
+            var divRM = document.getElementById(element.parentElement.parentElement.id + "Child");
 
-        // remove div after animation ended
-        divRM.addEventListener("animationend", (event) => {
-            divRM.remove();
-            img.remove();
-        });
+            // Add animation class
+            divRM.classList.add("rmAnim");
+
+            // run animation
+            divRM.style.animationPlayState = "running";
+
+            // remove div after animation ended
+            divRM.addEventListener("animationend", (event) => {
+                divRM.remove();
+            });
+        }
 
         // Change arrow up to arrow down
         element.classList.add("fa-square-caret-down");
@@ -127,6 +190,7 @@ function load_form_edit_info(isStud) {
         for (let i = 0; i < document.querySelectorAll('.infoDiv').length; i++) {
 
             mainDiv[i] = document.createElement('div');
+            mainDiv[i].classList.add('infoDiv');
             mainSpan[i] = document.createElement('span');
             mainSpan[i].innerHTML = infoKeys[i].innerHTML;
             mainSpan[i].classList.add('infoKey');
@@ -147,11 +211,15 @@ function load_form_edit_info(isStud) {
         }
         mainInput[0].autofocus = true;
 
+        // create Div for both buttons
+        const   butDiv = document.createElement('div');
+
         // Add save and cancel buttons
         const   save = document.createElement('button');
-        save.setAttribute('class', "btn btn-primary");
-        save.setAttribute('type', "submit");
+        save.setAttribute('class', 'btn btn-primary');
+        save.setAttribute('type', 'submit');
         save.setAttribute('id', 'saveData');
+        save.style.marginRight = '5px;';
         save.textContent = "Save";
 
         save.addEventListener('click', () => {
@@ -161,67 +229,79 @@ function load_form_edit_info(isStud) {
                 'username': document.getElementById('username').value,
                 'email': document.getElementById('email').value
             }
-            fetch("edit_data", {
-                method: 'PUT',
-                headers: {
-                    'content-Type': 'application/json',
-                    'X-CSRFToken': document.querySelector('meta[name="csrf-token"]').getAttribute('content'),
-                },
-                body: JSON.stringify(formData)
-            })
-            .then(response => {
-                if (response.ok) { console.log("HTTP request successful")}
-                else { console.log("HTTP request unsuccessful")}
-                return response.json();
-            })
-            .then(data => {
-                if (data.success) {
-                    // Show success message
-                    alert("You have successfully updated your data.");
-                }
-                else {
-                // show error msg
-                    const errors = [];
-                    for (const [field, messages] of Object.entries(data.errors)) {
-                        errors.push(`${field}: ${messages.join(", ")}`);
-                    }
-                        alert("Error: " + errors.join("\n"));
-                }
-            })
-            .catch(error => {
-                console.error("Error: " + error);
-                alert("Error: " + error);
-            });
             fetch("getUsernameConnected")
             .then(response => response.json())
             .then(user_connected => {
-                fetch(`user/${user_connected}/information`)
-                .then(response => response.json())
-                .then(user_info => {
-                    for (const key in user_info) {
-                        switch(key) {
-                            case 'First name':
-                                document.getElementById("First name").innerHTML = user_info[key];
-                                break;
-                            case 'Last name':
-                                document.getElementById("Last name").innerHTML = user_info[key];
-                                break;
-                            case 'Email':
-                                document.getElementById("Email").innerHTML = user_info[key];
-                                break;
-                            case 'Username':
-                                document.getElementById("Username").innerHTML = user_info[key];
-                                break;
-                            default:
-                                break;
-                        }
-                    }
-                    divData.style.display = 'block';
-                    newForm.remove();
+                fetch("edit_data", {
+                    method: 'PUT',
+                    headers: {
+                        'content-Type': 'application/json',
+                        'X-CSRFToken': document.querySelector('meta[name="csrf-token"]').getAttribute('content'),
+                    },
+                    body: JSON.stringify(formData)
                 })
-                .catch (err => {
-                    console.log(err);
-                    alert(err);
+                .then(response => {
+                    if (response.ok) { console.log("HTTP request successful")}
+                    else { console.log("HTTP request unsuccessful")}
+                    return response.json();
+                })
+                .then(data => {
+                    if (data.success) {
+                        // Show success message
+                        alert("You have successfully updated your data.");
+                        console.log(user_connected);
+                        fetch(`user/${document.getElementById('username').value}/information`)
+                        .then(response => response.json())
+                        .then(user_info => {
+                            for (const key in user_info) {
+                                switch(key) {
+                                    case 'First name':
+                                        document.getElementById("First name").innerHTML = user_info[key];
+                                        break;
+                                    case 'Last name':
+                                        document.getElementById("Last name").innerHTML = user_info[key];
+                                        break;
+                                    case 'Email':
+                                        document.getElementById("Email").innerHTML = user_info[key];
+                                        break;
+                                    case 'Username':
+                                        // update username in navbar
+                                        var updateUsername = document.getElementById('ownUsername');
+                                        const arrow = document.getElementById('arrowUsername');
+                                        updateUsername.innerHTML = user_info[key] + "  ";
+                                        updateUsername.append(arrow);
+
+                                        //update username in title
+                                        var updateTitle = document.querySelector('.title_div');
+                                        updateTitle.innerHTML = user_info[key] + " profile";
+
+                                        //update username in form
+                                        document.getElementById("Username").innerHTML = user_info[key];
+                                        break;
+                                    default:
+                                        break;
+                                }
+                            }
+                            divData.style.display = 'block';
+                            newForm.remove();
+                        })
+                        .catch (err => {
+                            console.log(err);
+                            alert(err);
+                        });
+                    }
+                    else {
+                    // show error msg
+                        const errors = [];
+                        for (const [field, messages] of Object.entries(data.errors)) {
+                            errors.push(`${field}: ${messages.join(", ")}`);
+                        }
+                            alert("Error: " + errors.join("\n"));
+                    }
+                })
+                .catch(error => {
+                    console.error("Error: " + error);
+                    alert("Error: " + error);
                 });
             })
             .catch (err => {
@@ -235,8 +315,18 @@ function load_form_edit_info(isStud) {
         cancel.setAttribute('class', "btn btn-primary");
         cancel.setAttribute('type', "submit");
         cancel.setAttribute('id', 'cancelData');
+        cancel.style.marginLeft = '5px';
         cancel.textContent = "Cancel";
-        newForm.append(save, cancel);
+
+        cancel.addEventListener('click', () => {
+            divData.style.display = 'block';
+            newForm.remove();
+            event.preventDefault();
+        })
+
+        butDiv.append(save, cancel);
+        butDiv.style.textAlign = 'center';
+        newForm.append(butDiv);
 
         infoDiv.append(newForm);
     }
